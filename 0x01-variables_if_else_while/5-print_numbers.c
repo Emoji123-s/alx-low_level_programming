27b227a939e596eb87aa906f64084dc0810339d8
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2023-06-17 15:35:16 -0700

    Prints the digits of base10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - start of program
+ *
+ * Return: 0 if successful and an error code otherwise
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	printf("\n");
+
+	return (0);
+}
