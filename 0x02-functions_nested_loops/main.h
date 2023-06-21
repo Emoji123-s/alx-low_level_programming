2268e287660b3ecc6eebdad82a0a3aff4550313e
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2023-06-21 07:35:00 -0700

    Prints the last digit of a number

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -23,3 +25,4 @@ int add(int, int);
 void print_to_98(int n);
 
 void print_times_table(int n);
+#endif
