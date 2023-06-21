9e187b670ff1fa2f969329273ad93dbf34b33004
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2023-06-21 15:37:02 -0700

    Prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,24 +5,31 @@
  */
 void times_table(void)
 {
-	int i, j, ansr;
+	int i, j, k;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
-			ansr = i * j;
-			if (ansr < 10)
+			k = j * i;
+			if (j == 0)
 			{
-				_putchar('0' + ansr);
+				_putchar(k + '0');
 			}
-			else
+
+			if (k < 10 && j != 0)
 			{
-				_putchar('0' + (ansr / 10));
-				_putchar('0' + (ansr % 10));
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
-			_putchar(',');
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
